78f892e10daded6118b6aa9984b7963557bc94ae
Alec Brickner <alecjbrick@hotmail.com>
2015-10-03 22:50:29 -0700

    Added warehouse

---
+++ Assets/Scripts/Box.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Box : MonoBehaviour {
+
+	public Vector3 belt1;
+	public Vector3 belt2;
+	private int side;
+
+	// Use this for initialization
+	void Start () {
+		side = 2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKeyDown) {
+			switchBelts ();
+		}
+	}
+
+	void switchBelts() {
+		switch (side) {
+		case 1:
+			side = 2;
+			transform.position = belt2;
+			break;
+		case 2:
+			side = 1;
+			transform.position = belt1;
+			break;
+		}
+	}
+}
--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Item : MonoBehaviour {
 
+	public string name;
+
 	// Use this for initialization
 	void Start () {
 	
